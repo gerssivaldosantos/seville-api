8ae85f89e2391302289bebe33711934907e82aeb
gerssivaldosantos <canalandows@gmail.com>
2022-10-16 15:50:30 -0300

    refactor: enhance login

--- src/useCases/user/Login/login.controller.ts
+++ src/useCases/user/Login/login.controller.ts
@@ -1,21 +1,20 @@
-import { Request, Response } from "express";
-import { LoginDTO } from "./login.dto";
-import { LoginUseCase } from "./login.service";
+import { Request, Response } from 'express'
+import { LoginDTO } from './login.dto'
+import { LoginUseCase } from './login.service'
+import { resultServiceDTO } from '@/@types/generic'
 
 export class LoginController {
-    constructor(
-        private readonly LoginUseCase: LoginUseCase) { }
+  constructor (
+    private readonly LoginUseCase: LoginUseCase) { }
 
-    async handle(request: Request, response: Response) {
-        try {
-            const token = await this.LoginUseCase.execute(request.body as LoginDTO)
-            return response.status(202).json({
-                token: token
-            })
-        } catch (error) {
-            return response.status(400).json({
-                error: error.message || 'Unexpected error'
-            })
-        }
+  async handle (request: Request, response: Response): Promise<Response> {
+    try {
+      const result: resultServiceDTO = await this.LoginUseCase.execute(request.body as LoginDTO)
+      return response.status(result.code).json(result)
+    } catch (error) {
+      return response.status(400).json({
+        error: 'Unexpected error'
+      })
     }
+  }
 }
--- src/useCases/user/Login/login.service.ts
+++ src/useCases/user/Login/login.service.ts
@@ -1,26 +1,41 @@
-import { LoginDTO } from "./login.dto";
+import { LoginDTO } from './login.dto'
 import bcrypt from 'bcryptjs'
-import { TokenProviderInterface } from "@/providers/token-provider.interface";
-import { UserRepositoryInterface } from "@/repositories/user-repository.interface";
+import { TokenProviderInterface } from '@/providers/token-provider.interface'
+import { UserRepositoryInterface } from '@/repositories/user-repository.interface'
+import { User } from '@/entities/user.entity'
+import { resultServiceDTO, IUseCase } from '@/@types/generic'
 
-export class LoginUseCase {
+export class LoginUseCase implements IUseCase {
+  constructor (
+    private readonly UserRepository: UserRepositoryInterface,
+    private readonly TokenProvider: TokenProviderInterface
+  ) { }
 
-    constructor(
-        private readonly UserRepository: UserRepositoryInterface,
-        private readonly TokenProvider: TokenProviderInterface
-    ) { }
-
-    public async execute(login: LoginDTO) {
-        try {
-            const userSearched = await this.UserRepository.customFindByEmail(login.email)
-            const passwordValid = await bcrypt.compare(login.password, userSearched.password)
-            if (!passwordValid) {
-                throw new Error('Invalid credentials')
-            }
-            return this.TokenProvider.create(userSearched.id)
-
-        } catch (error) {
-            throw new Error(error.message || 'Unexpected Error')
+  async execute (login: LoginDTO): Promise<resultServiceDTO> {
+    try {
+      const userSearched = await this.UserRepository.customFindByEmail(login.email)
+      if (!(userSearched instanceof User)) {
+        return {
+          code: 404,
+          message: 'User not Found'
         }
+      }
+      const passwordValid = await bcrypt.compare(login.password, userSearched.password)
+      if (!passwordValid) {
+        return {
+          code: 401,
+          message: 'Credentials not valid'
+        }
+      }
+      return {
+        code: 201,
+        message: '',
+        content: {
+          token: this.TokenProvider.create(userSearched.id as string)
+        }
+      }
+    } catch (error: any) {
+      throw new Error(error.message)
     }
+  }
 }
